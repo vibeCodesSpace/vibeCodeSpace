e84f0e405be3bbccf7b2e767b91fd51ab9bafb93
Mattjhagen <info@pacmacmobile.com>
2025-06-29 17:26:17 -0500

    fix: Remove routes.js import from server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { registerRoutes } from "./routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
